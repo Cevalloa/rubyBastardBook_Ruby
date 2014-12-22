1019fb68254e92b422a927e9174b8b6702121b10
Alex Cevallos <cevalloa@gmail.com>
2014-12-22 14:37:41 -0800

    Completed recursion factorial

--- Tweet Fetching/Working With Twitter API/recursion.rb
+++ Tweet Fetching/Working With Twitter API/recursion.rb
@@ -1,5 +1,5 @@
 def append(ary, n)
-	return ary if n < 0
+	return ary if n < 0 
 	ary << n
 	append(ary, n - 1)
 end
@@ -7,6 +7,42 @@ end
 returnd = append([], 3)
 puts returnd.to_s
 
+#3 added
+# append runs, 2 added
+
 #add first element to array
 #minus element, add that to array
 #keep minusing until below 0
+
+#start at 0
+#add number to 0
+#stop number hits n
+
+def reverse_append(ary, n)
+	return ary if n < 0
+
+	reverse_append(ary, n-1)
+	ary << n
+	ary
+end
+
+# define a recursive function that finds the factorial of a number
+#Multiply highest number with second highest number
+#keep multiplying until one is reached
+def recursive_factorial(number)
+	return 1 if number < 1
+
+	recursive_factorial(number -1) * number
+	
+end
+
+puts recursive_factorial(6)
+
+# (5) * 6
+# (4) * 5
+# (3) * 4
+# (2) * 3
+# (1) * 1
+
+
+
