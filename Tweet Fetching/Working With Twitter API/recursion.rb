b58a78e1942c1c3f6da1217cdde82397b8d90319
Alex Cevallos <cevalloa@gmail.com>
2014-12-27 22:22:49 -0800

    Preparing for merge sort recursion

--- Tweet Fetching/Working With Twitter API/recursion.rb
+++ Tweet Fetching/Working With Twitter API/recursion.rb
@@ -291,20 +291,45 @@ end
 
 puts "-" * 40
 
-def merge_sort(arr)
-	left, right, result = []
+def merge(left, right) #merges two sorted arrays together from lowest to highest
+	result = [] #end result array
+	while left.size > 0 and right.size > 0 #while the left and right array are over 0
+		if left[0] <= right[0]	#if the first left is less than the first right
+			result << left[0]	#then add the first left array to the result
+			left = left[1..-1]	# reduce the size of the left array..start it at the second and keep the last value
+		else
+			result << right[0]   #the right is bigger, so move the right to the result
+			right = right[1..-1]	#decrease the size of the right, start it at the second value and keep the last value
+		end
+	end  #ends when one of the arrays is not bigger than 0
 
-	if arr.size <= 1
+	if left.size > 0   #if the left one still has objects within it..
+		result.concat left	#add the rest of the left array onto the result
+	end
+	if right.size > 0	#if the right one still has objects within it..
+		result.concat right	#add the rest of the objects to the result
+	end
+
+	return result
+
+end
+
+def merge_sort(arr) #breaks them down, then calls merge to merge them together
+	left, right, result = [] #create 3 arrays
+
+	#if the array is less than or equal to 1, return it.. or else keep breaking!
+	if arr.size <= 1 #if the array size is less than 1, return array
 		return arr
 	else
-		middle = arr.size/2
-		left = arr[0..middle - 1]
-		right = arr[middle..-1]
+		middle = arr.size/2 #find middle spot of array
+		left = arr[0..middle - 1] #split left array, 0 to middle minus one
+		right = arr[middle..-1] #split right array, middle to last
 
-		left = merge_sort(left)
-		right = merge_sort(right)
+		left = merge_sort(left) # now call same method on left array
+		right = merge_sort(right) #now call same method on right array
 
-		if left.last <= right [0]
+		#once they are back from merge sort..
+		if left.last <= right [0] 
 			left.concat right
 			return left
 		end
@@ -315,9 +340,77 @@ def merge_sort(arr)
 	
 end
 
+#test with [2,3,1,4,5]
+middle = 2
+left = arr[0..1] => 2,3
+right = arr[2..-1 => 1,4,5
+
+left = merge_sort([2,3])
+	middle = 1
+	left = arr[0..0] => 2
+	right = arr[0..-1] => 3
+		left = merge_sort(2)
+			return 2
+		right = merge_sort(3)
+			return 3
+		if 2 <= 3
+			2.concat 3
+		return left
+	end
+	result = merge([2,3], nil)
+		#merge method entry
+		result = []
+		if left.size > 0
+			result.concat [2,3]
+		end
+		return => [2,3]
+		#back
+	return [2,3]
+right = merge_sort[1,4,5]
+	middle = 1
+	left = arr[0..0] => 1
+	right = arr[0..-1] => 4,5
+
+	left = merge_sort([1])
+		return [1]
+	right = merge_sort([4,5])
+		middle = 1
+		left = arr[0..0] => 4
+		right = arr[0..-1] => 5
+		return [4,5]
+	end
+	result = merge([1],[4,5])
+		#merge method
+		while left.size>0 && right.size > 0
+			if 1 > 4
+				result << 1
+				left = left[1..-1]
+			else
+			end
+		end
+
+		if right.size >0
+			results.concat right
+		end
+		return [1,4,5]
+		#end merge
+	return [1,4,5]
+result = merge([2,3], [1,4,5])
+	if 2 > 1
+	
+
+
 puts merge_sort([1,2,3,4,5,6,7,8], [9,10,11,12,13,14,15,16])
 
 
+#going over merge sort again
+#break down into bits of one
+#join bits, but sort them
+
+def merge_confirm(arry1, arry2)
+
+
+end
 
 
 
