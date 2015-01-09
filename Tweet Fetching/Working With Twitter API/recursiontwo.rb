19ac6687fb87b79dcd937f18ce1569bb5d629275
Alex Cevallos <cevalloa@gmail.com>
2015-01-09 14:49:14 -0800

    Added infomration on module to class call order

--- Tweet Fetching/Working With Twitter API/recursiontwo.rb
+++ Tweet Fetching/Working With Twitter API/recursiontwo.rb
@@ -143,7 +143,16 @@ class BST
 
 end
 
-
+#modules classes order
+module InterestBearing 
+	def calculate_interest
+puts "Placeholder! We're in module InterestBearing." end
+end
+class BankAccount include InterestBearing def calculate_interest
+puts "Placeholder! We're in class BankAccount."
+puts "And we're overriding the calculate_interest method!" end
+end
+account = BankAccount.new account.calculate_interest
 
 
 
