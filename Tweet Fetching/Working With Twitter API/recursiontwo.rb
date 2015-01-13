90ec69e4042da5475824367feb9a79f0d96b0bde
Alex Cevallos <cevalloa@gmail.com>
2015-01-12 20:40:26 -0500

    Added an example working with main

--- Tweet Fetching/Working With Twitter API/recursiontwo.rb
+++ Tweet Fetching/Working With Twitter API/recursiontwo.rb
@@ -185,4 +185,13 @@ class Stack
 		def unload take_from_stack
 end end
 
+#Simple explaination of "main"
+def talk
+puts "Hello"
+end
+puts "Trying 'talk' with no receiver..."
+talk
+puts "Trying 'talk' with an explicit receiver..." obj = Object.new
+obj.talk
+
 
